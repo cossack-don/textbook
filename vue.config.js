f369b0a0afc202aea1f5d19a87fbb00ae7c201e4
SergeyZhur <ser90593696@yandex.ru>
2021-01-23 20:45:19 +0300

    change vue config and router index

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    publicPath: "/textbook/",
-    // publicPath: process.env.NODE_ENV === 'production' ?
-    //   '/group/':
-    //   '/'
+    // publicPath: "/textbook/",
+    publicPath: process.env.NODE_ENV === 'production' ?
+      '/textbook/':
+      '/'
     // используется для продакшена '/group/' имя репозитория
 };
