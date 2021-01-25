17728918196a3e54b3a3ccd44da44a8832a9b466
SergeyZhur <ser90593696@yandex.ru>
2021-01-25 16:58:42 +0300

    add img in comp fetch

--- src/views/theoryJsPages/Fetch.vue
+++ src/views/theoryJsPages/Fetch.vue
@@ -1,92 +1,83 @@
 <template>
   <div>
+    <h2 class="template-title-component">Fetch</h2>
 
-      <h2 class="template-title-component">Fetch</h2>
-      
-        <div class="template-code-component-wrapper">
-            <code class="template-style-code">
-                let url = "http://ya.ru"  - по умолчанию гет запрос <br>
-                fetch возвращает промис<br>
-            </code>
-        </div>
+    <div class="template-code-component-wrapper">
+      <code class="template-style-code">
+        let url = "http://ya.ru" - по умолчанию гет запрос <br />
+        fetch возвращает промис<br />
+      </code>
+    </div>
 
-        <div class="template-code-component-wrapper">
-        <code class="template-style-code">
-            fetch(url).then( <br>
-                (response) => { <br>
-                    response.status <br>
-                    response.statusText <br>
-                    response.url<br><br>
+    <div class="template-code-component-wrapper">
+      <code class="template-style-code">
+        fetch(url).then( <br />
+        (response) => { <br />
+        response.status <br />
+        response.statusText <br />
+        response.url<br /><br />
 
-                    важные методы<br>
-                    response.text()<br>
-                    response.json()<br>
-                }
-            )
-        </code>
-        </div>
+        важные методы<br />
+        response.text()<br />
+        response.json()<br />
+        } )
+      </code>
+    </div>
 
-        <div class="template-code-component-wrapper">
-        <code class="template-style-code">
-            пример<br>
+    <div class="template-code-component-wrapper">
+      <code class="template-style-code">
+        пример<br />
 
-            fetch(url).then(<br>
-                (response) => {<br>
-                    return response.text();<br>
-                }<br>
-            )<br>
-            .then( (data) => {<br>
-                console.log(data)<br>
-            })<br>
-        </code>
-        </div>
+        fetch(url).then(<br />
+        (response) => {<br />
+        return response.text();<br />
+        }<br />
+        )<br />
+        .then( (data) => {<br />
+        console.log(data)<br />
+        })<br />
+      </code>
+    </div>
 
-        <div class="template-code-component-wrapper">
-        <code class="template-style-code">
-            пример<br>
-                const options = {<br>
-                    methid:'post',<br>
-                    headers:{},<br>
-                    body:'' что отправляется на сервер<br>
-                }<br><br>
+    <div class="template-code-component-wrapper">
+      <code class="template-style-code">
+        пример<br />
+        const options = {<br />
+        methid:'post',<br />
+        headers:{},<br />
+        body:'' что отправляется на сервер<br />
+        }<br /><br />
 
-                fetch(url,options).then(...);<br>
-        </code>
-        </div>
+        fetch(url,options).then(...);<br />
+      </code>
+    </div>
 
-        <div class="template-code-component-wrapper">
-        <code class="template-style-code">
-            пример<br>
-            response - ответ<br><br>
+    <div class="template-code-component-wrapper">
+      <code class="template-style-code">
+        пример<br />
+        response - ответ<br /><br />
 
-            fetch('./data.json')<br>
-            .then( (response) => {<br>
-            return response.json();<br>
-            })<br>
-            .then( (data) => {<br>
-                console.log(data)<br>
-            })<br>
-        </code>
-        </div>
+        fetch('./data.json')<br />
+        .then( (response) => {<br />
+        return response.json();<br />
+        })<br />
+        .then( (data) => {<br />
+        console.log(data)<br />
+        })<br />
+      </code>
+    </div>
 
-
-
-
-
-
-
-
-
-
+    <img
+      src="@/assets/img/img-fetch-async-await.jpg"
+      alt=""
+      class="template-image"
+    />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
