1324d32b9734bb86a08e27674e5e66c78e304f4a
SergeyZhur <ser90593696@yandex.ru>
2021-01-27 17:31:43 +0300

    add lnik for code pen for form json

--- src/views/theoryJsPages/Для отправки формы на fetch.vue
+++ src/views/theoryJsPages/Для отправки формы на fetch.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    Для отправки формы на fetch
+    Для отправки формы на fetch formdata-text формат
     https://codepen.io/impertubable/pen/poEMXdM?editors=1010
 
     <br />
     https://only-to-top.ru/blog/programming/2019-09-17-otpravka-dannyh-fetch-api.html
 
+    <br />
+    https://codepen.io/impertubable/pen/gOLYYOP?editors=1010 отправка формы в
+    формате json
     <router-link to="/fetch"> PAGE fetch</router-link>
   </div>
 </template>
