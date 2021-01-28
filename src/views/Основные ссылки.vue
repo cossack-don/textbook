e717ce41c677fe6ab1f8d4b40063e941cabe98e2
SergeyZhur <ser90593696@yandex.ru>
2021-01-28 10:54:15 +0300

    corr

--- src/views/Основные ссылки.vue
+++ src/views/Основные ссылки.vue
@@ -6,7 +6,7 @@
     <div class="wrapper-video-lesson">
       <!--  -->
       <div class="bg-lesson-one">
-        <h2>Vue - JS Курсы</h2>
+        <h2>Vue - курсы</h2>
 
         <ul class="mt-10">
           <li
@@ -20,7 +20,7 @@
       </div>
       <!--  -->
       <div class="bg-lesson-two">
-        <h2>JS - Курсы</h2>
+        <h2>Vue - учебники</h2>
 
         <ul class="mt-10">
           <li
@@ -33,6 +33,48 @@
         </ul>
       </div>
       <!--  -->
+      <div class="bg-lesson-three">
+        <h2>JS - курсы</h2>
+
+        <ul class="mt-10">
+          <li
+            v-for="(item, index) in arrayWithUrlsVideoThree()"
+            :key="index"
+            class="mb-10"
+          >
+            <a :href="item.url" target="_blank">{{ item.nameContent }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="bg-lesson-three">
+        <h2>JS - учебники</h2>
+
+        <ul class="mt-10">
+          <li
+            v-for="(item, index) in arrayWithUrlsVideoThree()"
+            :key="index"
+            class="mb-10"
+          >
+            <a :href="item.url" target="_blank">{{ item.nameContent }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="bg-lesson-three">
+        <h2>Полезные ссылки IT</h2>
+
+        <ul class="mt-10">
+          <li
+            v-for="(item, index) in arrayWithUrlsVideoThree()"
+            :key="index"
+            class="mb-10"
+          >
+            <a :href="item.url" target="_blank">{{ item.nameContent }}</a>
+          </li>
+        </ul>
+      </div>
+
       <div class="bg-lesson-three">
         <h2>Полезные ссылки</h2>
 
@@ -51,6 +93,16 @@
     разделить в сторе данный файл
     this.$store.state.urlsVideo.arrayUrlsVideoContent на 3 массива в этом файле
     разбить
+
+    <ul class="mt-10 all-links-video">
+      <li
+        v-for="(item, index) in arrayWithUrlsVideoThree()"
+        :key="index"
+        class="mb-10"
+      >
+        <a :href="item.url" target="_blank">{{ item.nameContent }}</a>
+      </li>
+    </ul>
     <div>
       <a
         href="https://vk.com/doc43471214_580412033?hash=835031fea6b1191794&dl=d6ca56282167aecbf7"
@@ -79,29 +131,22 @@
       https://codepen.io/impertubable/pen/xxENvxJ <br />
       https://www.youtube.com/channel/UCr_ImhIijOhCX7mUzBMFlaA <br />
       https://www.youtube.com/watch?v=e-E0UB-YDRk&t=7420s <br />
-      https://www.youtube.com/watch?v=jqp1PNApy3A <br>
+      https://www.youtube.com/watch?v=jqp1PNApy3A <br />
 
-
-https://itlogia.ru/article/10_resursov_dlya_izucheniya_vuejs_v_2020_godu
-      https://www.vuemastery.com/courses
-      https://www.vuemastery.com/
+      https://itlogia.ru/article/10_resursov_dlya_izucheniya_vuejs_v_2020_godu
+      https://www.vuemastery.com/courses https://www.vuemastery.com/
       https://scrimba.com/allcourses?price=free&q=vue
       https://scotch.io/courses/getting-started-with-vuejs/vue-events-build-a-counter
-
-      https://alligator.io/vuejs/
-      https://www.youtube.com/watch?v=4deVCNJq3qc
-      https://www.freecodecamp.org/news/vue-js-full-course/
-
-      vuejs
+      https://alligator.io/vuejs/ https://www.youtube.com/watch?v=4deVCNJq3qc
+      https://www.freecodecamp.org/news/vue-js-full-course/ vuejs
       https://ww1.underver.se/viewtopic.php?t=181417
-
       https://www.youtube.com/watch?v=CjXgoYo86yY
       https://www.youtube.com/watch?v=BIuQfQcptIU&list=PL4cUxeGkcC9g0MQZfHwKcuB0Yswgb3gA5&index=4
-
       https://v15.vuetifyjs.com/ru/
       https://store.vuetifyjs.com/products/vuetify-material-dashboard-pro/preview
       https://vuetifyjs.com/en/getting-started/wireframes/#examples
       https://v15.vuetifyjs.com/ru/framework/pre-made-layouts
+      https://www.youtube.com/channel/UC6eR_ndNgaTeE5t2Ud4ZiHw/playlists
     </div>
   </div>
 </template>
@@ -131,6 +176,7 @@ export default {
 .wrapper-video-lesson {
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
 }
 
 .bg-lesson-one {
@@ -160,5 +206,10 @@ export default {
 
 .mt-10 {
   margin-top: 10px;
+}
+
+.all-links-video {
+  display: flex;
+  justify-content: center;
 }
 </style>
