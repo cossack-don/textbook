737c70cc6f4ddc65bd3c7a7ed2958510a34ebea6
SergeyZhur <ser90593696@yandex.ru>
2021-02-08 17:04:08 +0300

    test

--- src/views/Мусор-ссылки-заметки.vue
+++ src/views/Мусор-ссылки-заметки.vue
@@ -1,26 +1,22 @@
 <template>
-  <div>Мусор-ссылки-заметки https://qna.habr.com/q/680146
-https://www.youtube.com/watch?v=QTklKBndYgg&feature=emb_logo
-https://www.youtube.com/watch?v=yWtmBz-HodM&feature=emb_logo
+  <div>
+    Мусор-ссылки-заметки https://qna.habr.com/q/680146
+    https://www.youtube.com/watch?v=QTklKBndYgg&feature=emb_logo
+    https://www.youtube.com/watch?v=yWtmBz-HodM&feature=emb_logo vue+php+mysql
+    посм как запросы и извлечение из бд происходит
+    https://coreui.io/vue-laravel/?affChecked=1#compare
+    https://quasar.dev/vue-components/skeleton
+    https://www.youtube.com/playlist?list=PLCMvV-acWe2DOE8ZBL2M5g2O70noa6A12
 
-vue+php+mysql посм как запросы и извлечение из бд происходит
-
-https://coreui.io/vue-laravel/?affChecked=1#compare
-https://quasar.dev/vue-components/skeleton
-https://www.youtube.com/playlist?list=PLCMvV-acWe2DOE8ZBL2M5g2O70noa6A12
-
-        <div>
+    <div>
       https://metanit.com/web/react/2.3.php props - eto ?
       https://qna.habr.com/q/680146 https://codepen.io/impertubable/pen/xxENvxJ
       - code pen строение обертки SPA
       https://store.vuetifyjs.com/products/vuetify-material-dashboard-pro/preview
       https://quasar.dev/start/pick-quasar-flavour
-
-
-https://www.youtube.com/playlist?list=PLmRNNqEA7JoOF9d6cMrhExfEUig-yE-gz
+      https://www.youtube.com/playlist?list=PLmRNNqEA7JoOF9d6cMrhExfEUig-yE-gz
       <div>
-        <a 
-        
+        <a
           href="https://vk.com/doc43471214_580412033?hash=835031fea6b1191794&dl=d6ca56282167aecbf7"
           >ТЗ НА ДЖУНА ДОКИ</a
         >
