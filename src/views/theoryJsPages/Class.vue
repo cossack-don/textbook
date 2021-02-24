3f3f8f11d3aba982603f2e86e91cdc96ba1ec7ee
SergeyZhur <ser90593696@yandex.ru>
2021-02-24 11:22:57 +0300

    add comp class

--- src/views/theoryJsPages/Class.vue
+++ src/views/theoryJsPages/Class.vue
@@ -1,30 +1,26 @@
 <template>
-  <div>class
-не досмотрел урок
-https://www.youtube.com/watch?v=BASquaxab_w&list=PLNkWIWHIRwMGLJXugVvdK7i8UagGQNaXD&index=9
-В классе можно задать один раз конструктор
-
-название класса
-      class Task {
-Конструктор
-          constructor() {
-
-          }
-
-          Методы
-          computed() {
-
-          }
-      }
+  <div>
+    class - класс это функция, шаблон геттеры и сетторы - в класс можно
+    добавлять , спец свойства внутри объекта работают как методы, а снаружи
+    класса, как свойства как и у объектов названия свойств и методов могут быть
+    определены динамически название класса Task class Task { Конструктор (может
+    быть один в одном классе) constructor(title = Task.getDefault(), isCompleted
+    = false) { this.title = 'lern es 6', this.isCompleted = isCompleted,
+    Task.counter +=1 названия гет и сет не должны совпадать с обычными
+    названиями конструктора и пишут _ оно после перезаписалось(свойство)
+    this._isCompleted = isCompleted, } Статические свойства - методы static
+    getDefault() { return 'tutle' } Методы complite() { this.isCompleted = true;
+    } get isCompl() { return this._isCompl === true ? 'go' : 'no' } } задаются
+    статические свойства до создания экземпляров классов и после создания класса
+    Task.counter = 0; let task = new Task('lern es6', false); let task2 = new
+    Task('lern es6+1', true) let 3 = new Task() обращение к методу complite()
+    task.complite() console.log(Task.counter); console.log(task);
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
