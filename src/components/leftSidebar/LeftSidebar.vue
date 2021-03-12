3ba1357279243c8f5df7ef0181a7b88cf889e494
SergeyZhur <ser90593696@yandex.ru>
2021-03-12 14:33:30 +0300

    add new link api data

--- src/components/leftSidebar/LeftSidebar.vue
+++ src/components/leftSidebar/LeftSidebar.vue
@@ -166,11 +166,23 @@
       <a href="https://github.com/" target="_blank">GitHub</a>
       <br /><br />
 
-       <a href="https://medium.com/@allaev/%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-%D0%B8-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BA-%D1%81%D0%BE%D0%B1%D0%B5%D1%81%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E-%D1%84%D1%80%D0%BE%D0%BD%D1%82%D0%B5%D0%BD%D0%B4-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%B0-%D0%BD%D0%B0-javascript-9058a75710a" target="_blank">Вопросы для собеса Front-end</a>
+      <a
+        href="https://medium.com/@allaev/%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-%D0%B8-%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BA-%D1%81%D0%BE%D0%B1%D0%B5%D1%81%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E-%D1%84%D1%80%D0%BE%D0%BD%D1%82%D0%B5%D0%BD%D0%B4-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%B0-%D0%BD%D0%B0-javascript-9058a75710a"
+        target="_blank"
+        >Вопросы для собеса Front-end</a
+      >
       <br /><br />
 
+      <a
+        href="http://typescript-lang.ru/docs/Basic%20Types.html"
+        target="_blank"
+        >Теория TS russ</a
+      >
+      <br /><br />
 
-       <a href="http://typescript-lang.ru/docs/Basic%20Types.html" target="_blank">Теория TS russ</a>
+      <a href="https://randomuser.me/" target="_blank"
+        >API c данными, подобие jsonplaceholder</a
+      >
       <br /><br />
       <router-link to="/" class="btn-home-page">Home PAGE</router-link>
     </div>
